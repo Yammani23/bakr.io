09e1560228953f42afcddc147cb0cf0efdf5d253
Yammani23 <shah.yammani23@gmail.com>
2022-01-07 23:09:34 +1100

    Added ui and express server
    * Added ui
    * Added express routes
    * Installed express

--- src/app.js
+++ src/app.js
@@ -1,39 +1,40 @@
 function hasBread(testString) {
-    var hasBread = false
-    const breadTypes = [
-        "sourdough",
-        "ciabatta",
-        "rye",
-        "bread"
-    ]
-    const splitString = testString.split(" ")
-    for (var item in splitString) {
-
-        if (breadTypes.indexOf(splitString[item].toLowerCase()) >-1) {
-            hasBread = true
-        }
+  var hasBread = null;
+  const breadTypes = [
+    //array for existing bread tyoes
+    "sourdough",
+    "ciabatta",
+    "rye",
+    "bread",
+  ];
+  const splitString = testString.split(" ");
+  for (var item in splitString) {
+    if (breadTypes.indexOf(splitString[item].toLowerCase()) > -1) {
+      hasBread = splitString[item];
 
     }
+  }
 
-    if (hasBread == true) {
-        console.log(testString, " :It has bread!")
-    }
-    else {
-        console.log(testString, " :It does not have bread")
-    }
+  return hasBread
 }
 
-hasBread("Hi, I am Yammani")
-hasBread("I like rye bread")
-hasBread("I like rice")
-hasBread("I like rye")
-hasBread("sourdough rye")
-hasBread("")
-hasBread("hi")
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.urlencoded());
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "ui/views/bread.html"));
+});
 
+app.post("/bread", function (req, res) {
+  const breadName = hasBread(req.body["input_string"]);
+  if (breadName) {
+    res.send("There is " + breadName);
+  } else {
+    res.send("There is no bread");
+  }
+});
 
-
-
-
-
+app.listen(3000);
+console.log("Server up on port 3000");
